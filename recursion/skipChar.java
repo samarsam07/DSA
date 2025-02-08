e50da965f5113f971f79b715942c104151948514
samar <samarmohd251@gmail.com>
2025-02-08 21:47:52 +0530

    add skipString and skipString2 methods to enhance string manipulation in skipChar class

--- recursion/skipChar.java
+++ recursion/skipChar.java
@@ -1,10 +1,12 @@
 package recursion;
 public class skipChar{
 public static void main(String[] args) {
-    String s="bacccaad";
+    String s="bcdappappleccd";
     
     
-    System.out.println(skip( s, 'a'));
+    // System.out.println(skip( s, 'a'));
+    // System.out.println(skipString(s, "apple"));
+    System.out.println(skipString2(s, "app", "apple"));
 }
 static String skip(String s,char c){
     if(s.isEmpty()){
@@ -16,5 +18,24 @@ static String skip(String s,char c){
     }else{
         return ch+ skip( s.substring(1), c);
     }
+}
+static String skipString(String s,String c){
+    if(s.isEmpty()){
+        return "";
+    }
+    if(s.startsWith(c)){
+        return skipString(s.substring(c.length()), c);
+    }else{
+        return s.charAt(0)+skipString(s.substring(1), c);
+    }
+}
+static String skipString2(String s,String skipString,String notSkip){
+if(s.isEmpty()){
+    return "";
+}
+if(s.startsWith(skipString) && !s.startsWith(notSkip)){
+    return skipString2(s.substring(skipString.length()), skipString, notSkip);
+}else{
+    return s.charAt(0)+skipString2(s.substring(1), skipString, notSkip);}
 }
 }
