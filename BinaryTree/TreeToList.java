529036d61ecc3a52b8d06021738210df18087166
samar <samarmohd251@gmail.com>
2025-07-25 21:14:57 +0530

    save

---
+++ BinaryTree/TreeToList.java
@@ -0,0 +1,18 @@
+public class TreeToList {
+    void flattenTree(TreeNode root){
+        TreeNode current=root;
+        while(current!=null){
+            if(current.left!=null){
+                TreeNode temp=current.left;
+                while(temp.right!=null){
+                    temp=temp.right;
+                }
+                temp.right=current.right;
+                current.right=current.left;
+                current.left=null;
+            }
+            current=current.right;
+        }
+
+    }
+}
