0293929095816dfaefd381877605eb07d8cf31f6
samar <samarmohd251@gmail.com>
2025-08-06 21:30:40 +0530

    Implement CutSegment and HouseRobbber classes with dynamic programming solutions

--- DynamicProgramming/CutSegment.java
+++ DynamicProgramming/CutSegment.java
@@ -1,4 +1,36 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class CutSegment {
+    public static int cutSegment(int n,int x,int y,int z){
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        int ans=dp(n,x,y,z,dp);
+        if (ans<0)return -1;
+        return ans;
+    }
+
+    private static int solve(int n, int x, int y, int z) {
+        if (n == 0) return 0;
+        if (n < 0) return Integer.MIN_VALUE;
+        int a=solve(n-x,x,y,z)+1;
+        int b=solve(n-y,x,y,z)+1;
+        int c=solve(n-z,x,y,z)+1;
+        return Math.max(a,Math.max(b,c));
+    }
+    private static int dp(int n,int x,int y,int z,int[] dp){
+        if (n == 0) return 0;
+        if (n < 0) return Integer.MIN_VALUE;
+        if(dp[n]!=-1)return  dp[n];
+        int a=dp(n-x,x,y,z,dp)+1;
+        int b=dp(n-y,x,y,z,dp)+1;
+        int c=dp(n-z,x,y,z,dp)+1;
+        dp[n]= Math.max(a,Math.max(b,c));
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(cutSegment(6,5,2,2));
+    }
 }
--- DynamicProgramming/HouseRobbber.java
+++ DynamicProgramming/HouseRobbber.java
@@ -1,4 +1,27 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class HouseRobbber {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1)return nums[0];
+        int[] dp1=new int[n];
+        int[] dp2=new int[n];
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+        int first=dp(nums,0,dp1,n-1);
+        int last=dp(nums,1,dp2,n);
+        return Math.max(first,last);
+
+    }
+    private static  int dp(int[] nums,int i,int[] dp,int end){
+        if(i>=end)return 0;
+        if(i==end-1)return nums[i];
+        if(dp[i]!=-1)return  dp[i];
+        int include=dp(nums,i+2,dp,end)+nums[i];
+        int exclude=dp(nums,i+1,dp,end);
+        dp[i]=Math.max(include,exclude);
+        return dp[i];
+    }
 }
